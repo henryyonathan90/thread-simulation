8020a20bb44e52ae8d8bd053af33632045943b9b
Henry Jonathan <henry.jonathan@mokapos.com>
2020-09-02 19:37:49 +0700

    fix renaming add sample for basic mono and flux use

---
+++ reactive-server/src/main/java/henry/jonathan/test/nio/reactive/server/controller/TestController.java
@@ -0,0 +1,52 @@
+package henry.jonathan.test.nio.reactive.server.controller;
+
+import henry.jonathan.test.nio.reactive.server.service.WebclientVsFeignclientService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/test")
+@Slf4j
+public class TestController {
+
+    @Autowired
+    private WebclientVsFeignclientService webclientVsFeignclientService;
+
+    @Autowired
+    private Scheduler schedulers;
+
+    @GetMapping("mono")
+    public Mono<Integer> mono() {
+        return Mono.just(0).publishOn(Schedulers.parallel()).map(n -> {
+            log.info("First map");
+            return n + 1;
+        }).map(n -> {
+            log.info("Second map");
+            return n + 2;
+        });
+    }
+
+    @GetMapping("flux")
+    public Mono<Long> flux() {
+        return Flux.range(0, 100)
+                .parallel(2)
+                .runOn(Schedulers.parallel())
+                .map(n -> {
+                    log.info("Log from " + n);
+                    return n;
+                })
+                .sequential()
+                .count();
+    }
+
+}
--- reactive-server/src/main/java/henry/jonathan/test/nio/reactive/server/controller/WebclientVsFeignclientController.java
+++ reactive-server/src/main/java/henry/jonathan/test/nio/reactive/server/controller/WebclientVsFeignclientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/test/")
+@RequestMapping("/api/vs/")
 public class WebclientVsFeignclientController {
 
   @Autowired
